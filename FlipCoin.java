cf65994cd993bf1ae6b9feb950098eacf91ff5b1
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-07 08:41:10 +0530

    added flipcoin solution

---
+++ FlipCoin.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.JavaPP_Day5;
+import java.util.Scanner;
+
+public class FlipCoin {
+
+    public static void main(String[] args){
+        double headCount =0;
+        double tailCount =0;
+        System.out.println("Enter no. of turns to flip coin: ");
+        Scanner sc = new Scanner(System.in);
+        double turns = sc.nextDouble();
+
+        for(int i=0; i<turns; i++){
+            double value = Math.random();
+
+            if(value < 0.5){
+                headCount++;
+            }else {
+                tailCount++;
+            }
+        }
+        double percentHead = Math.round((headCount/turns)*100);
+        double percentTail = Math(tailCount/turns)*100;
+
+        System.out.println("Percentage of Heads: "+percentHead+"%");
+        System.out.println("Percentage of Tails: "+percentTail+"%");
+    }
+}

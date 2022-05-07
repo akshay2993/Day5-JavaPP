ce335f2f22518e68b07ebfd5fb4dcf414fa263b4
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-07 10:37:37 +0530

    Added solution to check a no. is odd or even

---
+++ EvenOddCheck.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.JavaPP_Day5;
+
+import java.util.Scanner;
+
+public class EvenOddCheck {
+    static boolean check(int num) {
+        if(num %2 == 0){
+            return(true);
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a no. to check if it's even or odd: ");
+        int num = sc.nextInt();
+
+        System.out.println(check(num) ? "It is an even number." : "It is an odd number.");
+
+    }
+}
